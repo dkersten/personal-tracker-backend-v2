52aedea285c56bbc1d54d764bd7aa7d4b1d1e8e4
dkersten <dkersten1994@gmail.com>
2021-02-19 16:45:38 -0500

    set up basic routes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello tool')
-})
+app.get('/activities', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+   
+app.post('/activities', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+   
+app.patch('/activities/:activityId', (req, res) => {
+    return res.send('Received a PATCH HTTP method');
+});
+   
+app.delete('/activities/:activityId', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}!`)
